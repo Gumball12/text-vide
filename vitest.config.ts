f9e00fdf698981395e8b0592691be33b2bff8f35
Gumball12 <to@shj.rip>
2022-05-24 00:36:54 +0900

    fix: add root field to test only src directory

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
   test: {
     coverage: {
       reporter: ['text', 'lcov', 'html'],
